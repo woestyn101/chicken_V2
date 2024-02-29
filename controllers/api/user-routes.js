9237e20a8b58312ac90f73f8a7ce7b279e650e87
zachary chapman <zach.chapman24@gmail.com>
2024-02-28 19:19:27 -0500

    started user-routes

--- controllers/api/user-routes.js
+++ controllers/api/user-routes.js
@@ -1,7 +1,12 @@
+const { Model, DataTypes } = require('sequelize');
 const router = require('express').Router();
 const path = require('path');
 const { User, Recipe } = require('../../models');
 
+router.get('/', (req, res) => {
+  res.json(user);
+});
 
+router
 
 module.exports = router;
