2dd5afbbd27eed3c5c39110daae71d4cd26cad38
Marius Du Plooy <woestyn101@gmail.com>
2024-02-28 19:50:08 -0500

    handlebars home page created

--- controllers/homeRoutes.js
+++ controllers/homeRoutes.js
@@ -1,11 +1,11 @@
 
 const router = require("express").Router();
 // const { Project, User } = require('../models');
-const withAuth = require("../utils/auth");
+//const withAuth = require("../utils/auth");
 
 router.get("/", async (req, res) => {
   try {
-    res.render("main");
+    res.render("home");
   } catch (err) {
     console.error(err);
     res.status(500).json(err);
