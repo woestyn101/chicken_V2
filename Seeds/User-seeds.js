0dac651f39e103221f834c3a725a7ce0dac3290d
Ellie22302 <146311968+Ellie22302@users.noreply.github.com>
2024-03-04 18:50:28 -0500

    working on home handlebars and db

--- Seeds/User-seeds.js
+++ Seeds/User-seeds.js
@@ -2,15 +2,15 @@ const {User} = require('../models')
 const Users= [
     {
         username: 'A',
-        password: 12345,
+        password: 12345678,
       },
       {
         username: 'Ellie',
-        password: 12345,
+        password: 12345678,
       },
       {
         username: 'Test',
-        password: 12345,
+        password: 12345678,
       },
 ];
 const usersProducts = () => User.bulkCreate(Users);
