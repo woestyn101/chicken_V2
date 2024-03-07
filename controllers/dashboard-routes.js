19fe810ff8289daca155c2082eb69a6bd752cb20
Marius Du Plooy <woestyn101@gmail.com>
2024-03-06 19:44:56 -0500

    navbar fixed and welcomes user

--- controllers/dashboard-routes.js
+++ controllers/dashboard-routes.js
@@ -5,7 +5,30 @@ const withAuth = require('../utils/auth');
 
 // This is the 'get' route 
 router.get('/', withAuth, async (req, res) => {
-  res.render('dashboard');
+ 
+  try {
+    // Find the logged in user based on the session ID
+    const userData = await User.findByPk(req.session.user_id, {
+      attributes: { exclude: ['password'] },
+      include: [{ model: Recipe }],
+    });
+
+    
+
+    const user = userData.get({ plain: true });
+
+    res.render('dashboard', {
+      ...user,
+      logged_in: true
+    });
+  } catch (err) {
+    res.status(500).json(err);
+  }
+    
+
+   
+
+  
 });
 
 module.exports = router;
